0a334caffbf11f15d39171ac9f193e15b5fb043e
DamaMichaelYohanna <get2dama11@gmail.com>
2024-06-22 07:22:53 +0100

    updated database access

--- database.py
+++ database.py
@@ -4,13 +4,12 @@ import pathlib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlmodel import Session
+from sqlmodel import Session, SQLModel
 
-sql_db_path = os.path.join(pathlib.Path(__file__), "database.db")
+sql_db_path = os.path.join(pathlib.Path(__file__).parent, "database.db")
 sql_db_link = f"sqlite:///{sql_db_path}"
 
 engine = create_engine(sql_db_link, echo=True)
-
 
 def get_session():
     with Session(engine) as session:
